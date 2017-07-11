674fda188543e525aa3edc4d49ba12b27bd82c35
Eric Korolev <3254@mail.ru>
2017-07-11 10:48:12 +0500

    Add restrictions to upload

--- upload.php
+++ upload.php
@@ -2,10 +2,24 @@
 $target_dir = "";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
+$audioFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check file size
+define('MB', 1048576); // Определяем размер Мигабайта
+if ($_FILES["fileToUpload"]["size"] > 10 * MB) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($audioFileType != "mp3" && $imageFileType != "wav") {
+    echo "Sorry, only mp3 & wav files are allowed.";
     $uploadOk = 0;
 }
 
